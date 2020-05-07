341f00ffcfbb0164788dcd4faab77e2717a13146
hcastillaq <hcastillaq@gmail.com>
2020-05-07 17:25:23 -0500

    spadmin

--- src/app/modules/spadmin/spadmin.module.ts
+++ src/app/modules/spadmin/spadmin.module.ts
@@ -1,15 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { SpadminRoutingModule } from './spadmin-routing.module';
-import { HomeComponent } from './pages/home/home.component';
-
+import { SpadminRoutingModule } from "./spadmin-routing.module";
+import { HomeComponent } from "./pages/home/home.component";
+import { MaterialModule } from "src/app/material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { ToolbarComponent } from "src/app/components/toolbar/toolbar.component";
+import { SidenavComponent } from "src/app/components/sidenav/sidenav.component";
+import { ComponentesModule } from "src/app/components/componentes.module";
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    SpadminRoutingModule
-  ]
+    SpadminRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ComponentesModule,
+  ],
 })
-export class SpadminModule { }
+export class SpadminModule {}
